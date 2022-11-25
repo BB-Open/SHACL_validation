69d8efda0c2abf77872d3afe879cfc493264f339
volker <volker.jaenisch@inqbus.de>
2022-11-25 21:08:01 +0100

    Recursion

--- shacl/validate.py
+++ shacl/validate.py
@@ -3,41 +3,169 @@ from pathlib import Path
 
 import pyshacl
 
+import os
+
+import rdflib
+from rdflib import URIRef
+
+os.environ['http_proxy'] = 'http://localhost:3128'
+os.environ['https_proxy'] = 'http://localhost:3128'
+
+
+REMOVE_SPARQL = """
+PREFIX sh: <http://www.w3.org/ns/shacl#>
+prefix dct: <http://purl.org/dc/terms/>
+prefix skos: <http://www.w3.org/2004/02/skos/core#>
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix dcat: <http://www.w3.org/ns/dcat#>
+prefix dcatde: <http://dcat-ap.de/def/dcatde/>
+prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+prefix adms: <http://www.w3.org/ns/adms#>
+prefix owl: <http://www.w3.org/2002/07/owl>
+prefix schema: <http://schema.org/>
+prefix spdx: <http://spdx.org/rdf/terms#>
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix vcard: <http://www.w3.org/2006/vcard/ns#>
+prefix rdf4j: <http://rdf4j.org/schema/rdf4j#>
+prefix sesame: <http://www.openrdf.org/schema/sesame#>
+prefix fn: <http://www.w3.org/2005/xpath-functions#>
+
+DELETE {
+    ?node ?path ?value
+}
+WHERE
+{
+    ?res a sh:ValidationResult ;
+        sh:focusNode ?node ;
+        sh:resultPath ?path ;
+        sh:value ?value .
+}
+"""
+
+SHACL_RESULTS = """
+PREFIX sh: <http://www.w3.org/ns/shacl#>
+
+SELECT ?node ?path ?value ?severity
+WHERE
+{
+    ?res a sh:ValidationResult ;
+        sh:resultSeverity ?severity ;
+        sh:focusNode ?node ;
+        sh:resultPath ?path ;
+        OPTIONAL {
+           ?res sh:value ?value ;
+        } .
+}
+"""
+
+NUMBER_OF_DATASETS = """
+PREFIX dcat:   <http://www.w3.org/ns/dcat#>
+PREFIX dcatap: <http://data.europa.eu/r5r#>
+PREFIX dcatde: <http://dcat-ap.de/def/dcatde/>
+
+SELECT (COUNT(?s) AS ?count)
+ 
+WHERE
+{{
+    {}
+}}
+"""
+
 # Where are the files living
 BASE_DIR = Path('/home/volker/workspace/PYTHON5/SHACL_validation')
 
 # SHACL rules
 SHAPE_FILES = [
     'dcat-ap_2.1.1_shacl_shapes.ttl',
-    'dcat-ap-spec-german-additions.ttl',
-    'dcat-ap-spec-german-messages.ttl',
+#    'dcat-ap-spec-german-additions.ttl',
+#    'dcat-ap-spec-german-messages.ttl',
 ]
 
 # The data to be validated (In this case the postdam dataset)
 DATA_URL = 'first_1000.ttl'
-
-# Import the SHACL rules
-
-validator = pyshacl.rdfutil.load_from_source(str(BASE_DIR / 'shapes' / SHAPE_FILES[0]))
-
-if len(SHAPE_FILES) > 1:
-    for filename in SHAPE_FILES[1:]:
-        validator = pyshacl.rdfutil.load_from_source(str(BASE_DIR / 'shapes' / filename), g=validator)
+#DATA_URL = 'datenadler.ttl'
 
 
-graph_data = open(BASE_DIR / 'data' / DATA_URL, 'rb')
+def statistic( stage, graph ):
+    print(stage)
+    a = graph.query(NUMBER_OF_DATASETS.format('?s a dcat:Dataset'))
+    print('datasets: ', a.bindings[0]['count'])
+    a = graph.query(NUMBER_OF_DATASETS.format('?s a dcat:Distribution'))
+    print('distributions: ', a.bindings[0]['count'])
+    a = graph.query(NUMBER_OF_DATASETS.format('?s ?p ?o'))
+    print('nodes:', a.bindings[0]['count'])
 
 
-conforms, report_graph, report_text = pyshacl.validate(
-    data_graph=graph_data,
-    shacl_graph=validator,
-    ont_graph=str(BASE_DIR / 'shapes' / 'dcat-ap-de-imports.ttl'),
-    do_owl_imports=True,
-    ont_graph_format='turtle',
-    data_graph_format='turtle',
-    meta_shacl= False,
-)
+class Validator:
+    """Validator instance"""
 
-print(report_text)
+    def __init__(self):
+        self.first_run = True
+        # Import the SHACL rules
+        self.validator = pyshacl.rdfutil.load_from_source(str(BASE_DIR / 'shapes' / SHAPE_FILES[0]))
+
+        if len(SHAPE_FILES) > 1:
+            for filename in SHAPE_FILES[1:]:
+                self.validator = pyshacl.rdfutil.load_from_source(str(BASE_DIR / 'shapes' / filename), g=self.validator)
+
+        self.ont_graph = rdflib.graph.Graph()
+        self.ont_graph.parse(str(BASE_DIR / 'shapes' / 'dcat-ap-de-imports.ttl'))
+
+    def validate(self, graph_data):
+
+        if self.first_run :
+            conforms, report_graph, report_text = pyshacl.validate(
+                data_graph=graph_data,
+                shacl_graph=self.validator,
+                ont_graph=self.ont_graph,
+                do_owl_imports=True,
+                meta_shacl=False,
+            )
+        else:
+            conforms, report_graph, report_text = pyshacl.validate(
+                data_graph=graph_data,
+                shacl_graph=self.validator,
+                do_owl_imports=True,
+                meta_shacl=False,
+            )
+        self.first_run = False
+        return conforms, report_graph, report_text
+
+
+validator = Validator()
+
+graph = rdflib.graph.Graph()
+graph_data = graph.parse(open(BASE_DIR / 'data' / DATA_URL, 'rb'))
+
+conforms = False
+steps = 1
+
+while not conforms:
+    conforms, report_graph, report_text = validator.validate(graph_data)
+
+    print(f'\nstep: {steps}')
+    steps += 1
+
+    statistic('\nInput Data: ', graph_data)
+
+    print('\nViolations: ', len([i for i in report_graph.triples((None, None, URIRef('http://www.w3.org/ns/shacl#ValidationResult')))]))
+
+    print(report_text)
+
+    shacl_results = report_graph.query(SHACL_RESULTS)
+
+    for shacl_result in shacl_results.bindings:
+        if 'value' in shacl_result:
+            graph_data.remove((shacl_result['node'], shacl_result['path'], shacl_result['value']) )
+        else:
+            graph_data.remove((shacl_result['node'], None, None) )
+
+    graph_data.commit()
+
+    statistic('\nValidated Data', graph_data)
+
+
+
+graph_data.serialize(BASE_DIR / 'data' / 'validated_output.ttl', 'turtle')
 
 sys.exit(0)
